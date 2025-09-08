7aaca7d762c26b87dfa6d03aa0c9c8ccf70f28bf
Hesam <itshesamhoseini@gmail.com>
2025-09-08 10:39:26 +0200

    Add data array to project

--- main.sh
+++ main.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+family_users=()
+number_of_inbound=1
+count=$(jq ".inbounds[$number_of_inbound].settings.clients | length" /usr/local/x-ui/bin/config.json)
+json=$(jq ".inbounds[$number_of_inbound].settings.clients" /usr/local/x-ui/bin/config.json)
+
+last_index=$(($count - 1));
+
+for email_row in $(seq 0 $last_index) ;do
+		uuid=$(echo $json | jq ".[$email_row].id");
+		email$(echo $json | jq ".[$email_row].email");
+done
