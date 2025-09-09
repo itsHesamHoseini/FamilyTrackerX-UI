45a21bfd4929b890427ec2b45f7d6b782769b405
Hesam <itshesamhoseini@gmail.com>
2025-09-10 01:00:39 +0200

    A File Created

--- main.sh
+++ main.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 family_users=()
+blocked_sites=('.phncdn.com' 'xnxx.com' 'pornhub.com' 'xhamster.com' 'xvideos.com')
 number_of_inbound=1
 config_json_address="/usr/local/x-ui/bin/config.json"
 userslogs="/usr/local/x-ui/access.log"
@@ -37,11 +38,19 @@ returnofgrep=$(grep -E "\<($pattern)\>" $userslogs);
 
 first_line=$(echo "$returnofgrep" | head -n 1 | awk '{print $1,$2}')
 last_line=$(echo "$returnofgrep" | tail -n 1 | awk '{print $1,$2}')
-echo "$first_line"
-echo "$last_line"
+first_line_unixtime=$(date -d "$first_line" +"%s")
+last_line_unixtime=$(date -d "$last_line" +"%s")
 
-# grep $emails "$userslogs"
+# echo $first_line_unixtime
+# echo $last_line_unixtime
+# echo $(($last_line_unixtime - $first_line_unixtime))
 
+while IFS='' read -r line; do
+	# for blocked_site in $(seq 0 $((${#blocked_sites[@]} - 1 ))) ;do
+	for blocked_site in "${blocked_sites[@]}" ;do
+		echo "$line" | awk '{print $6}' | grep "$blocked_site" && awk '{print "Users :$11 - sites: $6"}'
+	done
+done < $userslogs
 
 # IFS='|'
 # for e in "${data[@]}" ; do
